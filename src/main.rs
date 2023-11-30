9eabd1d67b000b6f2f73ee28cb48e53a986e2caf
Andy Clegg <andrew.clegg@pacerevenue.com>
2023-11-30 09:12:45 +0000

    Can launch browser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::env;
-use subprocess::Popen;
+use subprocess::{Popen,PopenConfig};
 
 fn launch_browser(profile_dir: &str, url: Option<&str>) {
-    let command: Vec<String> = vec!["/usr/bin/flatpak".to_string(),"run".to_string(),"--branch=stable".to_string(),"--arch=x86_64".to_string(),"--command=/app/bin/chrome".to_string(),"--file-forwarding".to_string(),"com.google.Chrome".to_string(),format!("--profile-directory={}", profile_dir)];
+    let profile_dir_arg = &format!("--profile-directory={}", profile_dir);
+    let mut command: Vec<&str> = vec!["/usr/bin/flatpak","run","--branch=stable","--arch=x86_64","--command=/app/bin/chrome","--file-forwarding","com.google.Chrome", profile_dir_arg];
 
     if url.is_some() {
-        command.push(url);
+        command.push(url.unwrap());
     }
+
+    Popen::create(&command, PopenConfig::default()).expect("Flatpak should be runnable").wait().expect("Chrome should launch cleanly");
 }
 
 
@@ -14,5 +17,5 @@ fn launch_browser(profile_dir: &str, url: Option<&str>) {
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(args);
-    launch_browser("Default", Some("www.github.com"))
+    launch_browser("Profile 2", Some("www.github.com"))
 }
